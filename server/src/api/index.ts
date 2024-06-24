c26debeba4808521f23b856aed918462c3091db5
aksinghania <akshtsinghnia@gmail.com>
2024-06-25 01:39:27 +0530

    modified api

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,36 +1,51 @@
-import express from 'express';
-import axios from 'axios';
+import express from "express";
+import axios from "axios";
 
-
-import MessageResponse from '../interfaces/MessageResponse';
-import emojis from './emojis';
+import MessageResponse from "../interfaces/MessageResponse";
+import emojis from "./emojis";
 
 const router = express.Router();
 
-router.get<{}, MessageResponse>('/', (req, res) => {
+router.get<{}, MessageResponse>("/", (req, res) => {
   res.json({
-    message: 'API - 👋🌎🌍🌏',
+    message: "API - 👋🌎🌍🌏",
   });
 });
 
-router.use('/emojis', emojis);
+router.use("/emojis", emojis);
 
 // /api/v1/stats/U0217R0029Z
 // https://arcade-leaderboard-kr5n0x8n2-akshatsinghanias-projects-a4067bab.vercel.app/api/v1/stats/U0217R0029Z
-router.get('/stats/:username', async (req, res) => {
+router.get("/stats/:username", async (req, res) => {
   const username = req.params.username;
   const url = `https://hackhour.hackclub.com/api/stats/${username}`;
 
   try {
     const response = await axios.get(url, {
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
     res.json(response.data);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Error fetching data' });
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Error fetching data" });
+  }
+});
+router.get("/history/:username", async (req, res) => {
+  const username = req.params.username;
+  const url = `https://hackhour.hackclub.com/api/history/${username}`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Error fetching data" });
   }
 });
 
